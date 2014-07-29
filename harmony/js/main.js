c1032538e38831a280ed3caf92c5ecab89808b10
harshabhat86 <harshabhat86@gmail.com>
2014-07-29 08:50:48 -0700

    Jpeg Encoder added.

--- harmony/js/main.js
+++ harmony/js/main.js
@@ -421,7 +421,8 @@ function onMenuExportImage()
 {
 	// window.open(canvas.toDataURL('image/png'),'mywindow');
 	flatten();
-	var image = flattenCanvas.toDataURL("image/png");
+	var image = flattenCanvas.toDataURL("image/png").replace("image/png", "image/octet-stream");
+    //Content-Disposition: attachment; filename="MyFileName.ZIP"
     window.location.href=image;
 //	window.open(flattenCanvas.toDataURL('image/png'),'mywindow');
 }
