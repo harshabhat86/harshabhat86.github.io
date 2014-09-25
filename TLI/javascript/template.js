2fe015121e0b2b79ce4a6511f6e41c09311726b9
harshabhat86 <harshabhat86@gmail.com>
2014-09-25 02:13:28 -0700

    TLI initialized
    Google Maps integrated

--- TLI/javascript/template.js
+++ TLI/javascript/template.js
@@ -1,2 +1,31 @@
-$(".tour").click(function(){window.open("timeline/timeline.html","_self");})
+var lat, lng;
+
+function initialize() {
+
+
+var mapCanvas = document.getElementById('map_canvas');
+
+var mapOptions = {
+    //12.58,77.38
+      center: new google.maps.LatLng(parseFloat(lat),parseFloat(lng)),
+      zoom: 8,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    }
+    var map = new google.maps.Map(mapCanvas, mapOptions);    
+
+};
     
+function set_location(pos){
+    lat = pos.coords.latitude;
+    lng = pos.coords.longitude;
+};
+
+function getLocation() {
+    if (navigator.geolocation) {
+        position = navigator.geolocation.getCurrentPosition(set_location);
+    } else { 
+        console.log("No Geo Location");
+    }
+}
+getLocation();
+google.maps.event.addDomListener(window, 'load', initialize);
