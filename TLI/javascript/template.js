d3deb61e88bed3b1429dbefbfb924e95c731903a
harshabhat86 <harshabhat86@gmail.com>
2014-09-25 15:35:22 -0700

    Map Co-ordinates added for demo

--- TLI/javascript/template.js
+++ TLI/javascript/template.js
@@ -1,5 +1,37 @@
 var lat, lng;
 
+var locationArray = [{area:"Banashankari, Bangalore",lat:12.9373, lng:77.5543},
+                      {area:"RVCE, Bangalore",lat:12.9239, lng:77.4997},
+                     {area:"Malleshwaram, Bangalore",lat:12.9800, lng:77.5750},
+                     {area:"Maratahalli, Bangalore",lat:12.9562, lng:77.7019},
+                     {area:"KR Puram, Bangalore",lat:12.9950, lng:77.6800},
+                     {area:"Basavanagudi, Bangalore",lat:12.9400, lng:77.5700}
+                     
+                    ];
+var coordArray = [];
+
+var mapPosition = function(){
+    var i=0;
+    for (i=0;i<locationArray.length;i++){
+     coordArray.push(new google.maps.LatLng(locationArray[i].lat,locationArray[i].lng));      
+    }
+    
+}
+var showAllCoordinates = function(myMap){
+    mapPosition();
+    var coord;
+  for (coord in coordArray) {
+    new google.maps.Marker({
+      position: coordArray[coord],
+      map: myMap,
+      title: locationArray[coord].area
+    });
+  }
+}
+
+/*
+This is a method that initializes google maps in the web page
+*/
 function initialize() {
 
 
@@ -7,12 +39,13 @@ var mapCanvas = document.getElementById('map_canvas');
 
 var mapOptions = {
     //12.58,77.38
-      center: new google.maps.LatLng(12.58,77.38),
-      zoom: 8,
+      center: new google.maps.LatLng(12.9667,77.5667),
+      zoom: 10,
       mapTypeId: google.maps.MapTypeId.ROADMAP
     }
     var map = new google.maps.Map(mapCanvas, mapOptions);    
 
+    showAllCoordinates(map);
 };
     
 function set_location(pos){
@@ -27,5 +60,8 @@ function getLocation() {
         console.log("No Geo Location");
     }
 }
+
+//Making it global intentionally. This is temp change
+
 getLocation();
 google.maps.event.addDomListener(window, 'load', initialize);
