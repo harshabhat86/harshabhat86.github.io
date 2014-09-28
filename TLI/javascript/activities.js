5b2cabe5ef560fa1c8d81d328f12a3ff29ec7340
harshabhat86 <harshabhat86@gmail.com>
2014-09-28 12:23:15 -0700

    Person Profile and activities page

--- TLI/javascript/activities.js
+++ TLI/javascript/activities.js
@@ -1,4 +1,12 @@
-function getData() {
+var userid = "S55RBrto3KV4uycw69TepJlO1";
+var username = "Harsha Bhat";
+
+function loaded() {
+    getData("owned");
+    getData("contributed");
+}
+
+function getData(tab) {
     if (window.XMLHttpRequest) {
         // code for IE7+, Firefox, Chrome, Opera, Safari
         var xmlhttp = new XMLHttpRequest();
@@ -10,10 +18,18 @@ function getData() {
     xmlhttp.onreadystatechange = function () {
         if (xmlhttp.readyState === 4 && xmlhttp.status === 200) {
             try {
-                console.log(xmlhttp.responseText);
                 var data = JSON.parse(xmlhttp.responseText);
-                console.log(data);	
-                appendText(data);
+                
+                switch(tab) {
+                    case "owned" : {
+                        appendOwnedData(data);
+                        break;
+                    }
+                    case "contributed" : {
+                        appendContributedToData(data);
+                        break;
+                    }
+                }
             } catch (x) {
                 console.log("Crashed " + x);
             }
@@ -21,11 +37,22 @@ function getData() {
         } 
     }
     
-    var myurl = encodeURIComponent("{\"ownerName\":\"Sharath M Bhat\"}");
-    console.log(myurl);
+    var myurl;
+    var myOtherUrl;
+    
+    switch(tab) {
+            case "owned" : {
+                myurl = encodeURIComponent("{\"ownerId\":\""+userid+"\"}");
+                myOtherUrl = "https://api.parse.com/1/classes/Spotfix?where="+myurl;
+                break;
+            }
+            case "contributed" : {
+                myurl = encodeURIComponent("{\"volunteers\":\""+username+"\"}");
+                myOtherUrl = "https://api.parse.com/1/classes/Spotfix?where="+myurl;
+                break;
+            }
 
-    var myOtherUrl = 
-       "https://api.parse.com/1/classes/Spotfix?where="+myurl;
+    }
     
     xmlhttp.open("GET", myOtherUrl, true);
     xmlhttp.setRequestHeader("X-Parse-Application-Id", "vSo04vEpbyviumU65MZFyuaj8zPx0aOhBfpTrAB0")
@@ -33,77 +60,67 @@ function getData() {
     xmlhttp.send();
 }
 
-function appendText(data) {
+function appendOwnedData(data) {
     var results = data.results;
-    console.log("Results[0] = "+results[0].addressOfTheFix);
-    
     var size = results.length;
     
     for(var i = 0; i < size; i++) {
-        var refId = "collapse"+i;
+        var refId = "collapse-owned"+i;
+        var completed = results[i].isComplete? 'Completed' : 'To be fixed';
         
         $( "#accordion-owned-data" ).append(
             '<div class="panel panel-default">' +
             '<div class="panel-heading">' +
             '<h4 class="panel-title">' +
             '<a data-toggle="collapse" data-parent="#accordion-owned-data" href="#' + refId + '">' +
-            results[i].ownerName + ', Hours needed : ' + results[i].hoursNeeded + 
+            results[i].addressOfTheFix + ' --- Date : ' + results[i].soptFixDate.iso.substring(0, 10) + ' --- ' + completed +
             '</a></h4></div>' +
             '<div id="' + refId + '" class="panel-collapse collapse">' +
-            '<div class="panel-body"> Volunteers : ' +
-            results[i].volunteers +
+            '<div class="panel-body">' +
+            '<b>' + results[i].description + '</b><br/>' +
+            'To be fixed on                : ' + results[i].soptFixDate.iso.substring(0, 10) + '<br/>' +
+            'Approximate time for fix      : ' + results[i].hoursNeeded + '<br/>' +
+            'People needed                 : ' + results[i].noOfPeople + '<br/>' +
+            'Volunteers                    : ' + results[i].volunteers + '<br/>' +
+            'Location(latitude, longitude) : ' + results[i].latLng.latitude + ', ' + results[i].latLng.longitude + '<br/>' +
+            'Created on                    : ' + results[i].createdAt.substring(0, 10) + '<br/>' +
+            'Last updated on               : ' + results[i].updatedAt.substring(0, 10) + '<br/>' +
             '</div></div>'
         );
     }
     
-//    $( "#accordion-owned-data" ).append(
-//            '<div class="panel panel-default">' +
-//            '<div class="panel-heading">' +
-//            '<h4 class="panel-title">' +
-//            '<a data-toggle="collapse" data-parent="#accordion-owned-data" href="#collapseOne">' +
-//            results[0].ownerId +
-//            '</a></h4></div>' +
-//            '<div id="collapseOne" class="panel-collapse collapse in">' +
-//            '<div class="panel-body">' +
-//            results[0].objectId +
-//            '</div></div>'
-//    );
-//    $( "#accordion-owned-data" ).append(
-//            '<div class="panel panel-default">' +
-//            '<div class="panel-heading">' +
-//            '<h4 class="panel-title">' +
-//            '<a data-toggle="collapse" data-parent="#accordion-owned-data" href="#collapseTwo">' +
-//            results[1].ownerId +
-//            '</a></h4></div>' +
-//            '<div id="collapseTwo" class="panel-collapse collapse">' +
-//            '<div class="panel-body">' +
-//            results[1].objectId +
-//            '</div></div>'
-//    );
     $( "#accordion-owned-data" ).addClass("panel-group")
-    $( "#accordion-owned-data" ).accordion( "refresh" );
+}
 
-//    $("input").click( function() {
-//       $( "#accordion" ).find($( "#accordion" ).accordion( "option", "header" )).eq(this.min).find($("span.accordion-header")).text("Section " + Math.random());
-//    });
-//    
-//    <div class="panel panel-default">
-//        <div class="panel-heading">
-//            <h4 class="panel-title">
-//                <a data-toggle="collapse" data-parent="#accordion" href="#collapseOne" id="">1. What is HTML?</a>
-//            </h4>
-//        </div>
-//        <div id="collapseOne" class="panel-collapse collapse in">
-//            <div class="panel-body">
-//                <p>HTML stands for HyperText Markup Language. HTML is the main markup language for describing the structure of Web pages. <a href="http://www.tutorialrepublic.com/html-tutorial/" target="_blank">Learn more.</a></p>
-//            </div>
-//        </div>
-//    </div>
-                                    
-                                    
-//    var txt1 = "<p>Text.</p>";               // Create element with HTML  
-//    var txt2 = $("<p></p>").text("Text.");   // Create with jQuery
-//    var txt3 = document.createElement("p");  // Create with DOM
-//    txt3.innerHTML = "Text.";
-//    $("#accordion-owned-data").append(txt1, txt2, txt3);         // Append the new elements 
+function appendContributedToData(data) {
+    var results = data.results;
+    var size = results.length;
+    
+    for(var i = 0; i < size; i++) {
+        var refId = "collapse-contributed"+i;
+        var completed = results[i].isComplete? 'Completed' : 'To be fixed';
+        
+        $( "#accordion-contributed-data" ).append(
+            '<div class="panel panel-default">' +
+            '<div class="panel-heading">' +
+            '<h4 class="panel-title">' +
+            '<a data-toggle="collapse" data-parent="#accordion-contributed-data" href="#' + refId + '">' +
+            results[i].addressOfTheFix + ' --- Date : ' + results[i].soptFixDate.iso.substring(0, 10) + ' --- ' + completed +
+            '</a></h4></div>' +
+            '<div id="' + refId + '" class="panel-collapse collapse">' +
+            '<div class="panel-body">' +
+            '<b>' + results[i].description + '</b><br/>' +
+            'To be fixed on                : ' + results[i].soptFixDate.iso.substring(0, 10) + '<br/>' +
+            'Approximate time for fix      : ' + results[i].hoursNeeded + '<br/>' +
+            'People needed                 : ' + results[i].noOfPeople + '<br/>' +
+            'Owner name                    : ' + results[i].ownerName + '<br/>' +
+            'Volunteers                    : ' + results[i].volunteers + '<br/>' +
+            'Location(latitude, longitude) : ' + results[i].latLng.latitude + ', ' + results[i].latLng.longitude + '<br/>' +
+            'Created on                    : ' + results[i].createdAt.substring(0, 10) + '<br/>' +
+            'Last updated on               : ' + results[i].updatedAt.substring(0, 10) + '<br/>' +
+            '</div></div>'
+        );
+    }
+    
+    $( "#accordion-contributed-data" ).addClass("panel-group")
 }
--- TLI/javascript/home.js
+++ TLI/javascript/home.js
@@ -1,19 +1,35 @@
 Parse.initialize("vSo04vEpbyviumU65MZFyuaj8zPx0aOhBfpTrAB0",
 	"kz4KisAWl3Xc8QRWbgpVU2FrnGoMh5BIL0I3V0c1");
 
-$('#home_Image').height($(window).height()) ;
-$('#home_Image').width($(window).width()) ;
+$(document).ready(function(){
+        $('#home_Image').height($(window).height()) ;
+        $('#home_Image').width($(window).width()) ;
 
-$('.carousel_image').height($(window).height()) ;
-$('.carousel_image').width($(window).width()) ;
+        $('.carousel_image').height($(window).height()) ;
+        $('.carousel_image').width($(window).width()) ;
+    
+        $(window).on('resize',function(){
+        $('.carousel_image').height($(window).height()) ;
+        $('.carousel_image').width($(window).width()) ;
+        $('#home_Image').height($(window).height()) ;
+        $('#home_Image').width($(window).width()) ;
+            
+        
+});
+    
+//    $("#glyph").on('click',function(e){
+//            e.preventDefault();
+//            $("body, html").animate({ 
+//            scrollTop: $( "#carousel-example-generic" ).offset().top 
+//        }, 600);
+//        }              
+//        );    
 
-$(window).on('resize',function(){
-    $('.carousel_image').height($(window).height()) ;
-    $('.carousel_image').width($(window).width()) ;
-    $('#home_Image').height($(window).height()) ;
-    $('#home_Image').width($(window).width()) ;
     
 });
+
+
+
 
 /*
     var _top = $(window).scrollTop();
