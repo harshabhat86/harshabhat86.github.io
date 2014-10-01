60b3c10838ef4c06c7ceee44a5adb3281a61c922
harshabhat86 <harshabhat86@gmail.com>
2014-09-30 23:55:42 -0700

    Map integration done for spotfix.html

--- TUG/javascript/spotfix.js
+++ TUG/javascript/spotfix.js
@@ -1,5 +1,167 @@
 var userid = Parse.User.current().attributes.username;
 var username = Parse.User.current().attributes.name;
+locationArray = [];
+
+/*
+Harsha:Adding Google Map first.
+
+*/
+
+//var locationArray = [{
+//        area: "Banashankari, Bangalore",
+//        lat: 12.9373,
+//        lng: 77.5543,
+//        desc: bigFatDescription
+//    },
+//    {
+//        area: "RVCE, Bangalore",
+//        lat: 12.9239,
+//        lng: 77.4997,
+//        desc: bigFatDescription
+//    },
+//    {
+//        area: "Malleshwaram, Bangalore",
+//        lat: 12.9800,
+//        lng: 77.5750,
+//        desc: bigFatDescription
+//    },
+//    {
+//        area: "Maratahalli, Bangalore",
+//        lat: 12.9562,
+//        lng: 77.7019,
+//        desc: bigFatDescription
+//    },
+//    {
+//        area: "KR Puram, Bangalore",
+//        lat: 12.9950,
+//        lng: 77.6800,
+//        desc: bigFatDescription
+//    },
+//    {
+//        area: "Basavanagudi, Bangalore",
+//        lat: 12.9400,
+//        lng: 77.5700,
+//        desc: bigFatDescription
+//    }
+//
+//    ];
+
+/*
+
+This function takes the data received from PArse and prepares an apt description to be shown.
+*/
+
+var getDescription = function(obj){
+    var arr = [];
+    
+    
+    arr.push("<b>"+obj.description+"</b><br>");
+    arr.push("<em>"+obj.hoursNeeded+" hours needed by "+obj.noOfPeople+" people </em><br>");
+    arr.push("Initiated by "+obj.ownerName+"  and scheduled on <b>"+obj.soptFixDate.iso.substr(0,10)+"</b><br>");
+    
+    arr.push("<b>Address:</b> "+obj.addressOfTheFix==undefined?'Not Given':obj.addressOfTheFix);
+    return arr.join("\n");
+}
+
+/*
+Takes Latitude and Longitude converts it into Google's format.
+*/
+var coOrdToGooglePoints = function (location) {
+    var i = 0;
+    var coOrdArray=[];
+    for (i = 0; i < location.length; i++) {
+        coOrdArray.push(new google.maps.LatLng(location[i].latLng.latitude, location[i].latLng.longitude));
+    }
+    return coOrdArray;
+
+}
+
+/*
+Function: drawGoogleMaps
+@input: 
+
+map: A map object by google. Need a reference of the map.
+
+geoLocationArray. Type: Array of objects of the type {latitude, longitude}
+example input: [{lat:latitude1,lng:longitude1},{lat:latitude2,lng:longitude2},{lat:latitude3,lng:longitude3}....]
+
+
+Output: None.
+
+It will draw google map with geo points tagged
+*/
+var pinCoordinates = function(myMap,geoLocationArray){
+    
+    var coordinates = [];
+    coordinates = coOrdToGooglePoints(geoLocationArray);
+    var coord;
+    var count=0;
+    for (coord in coordinates) {
+        
+        var markr = new google.maps.Marker({
+            position: coordinates[coord],
+            map: myMap,
+            title: geoLocationArray[count].description+": [At: "+coordinates[coord].toString()+"]"
+            
+        });
+        count+=1;
+
+        /*
+        Binding the co ordinate values using closures!
+        */
+        google.maps.event.addListener(markr, 'click', function (mapObj, marker, info) {
+            
+            return function(){
+            var infowindow = new google.maps.InfoWindow({
+
+                content: '<b>'+info+'</b>'
+            });
+            infowindow.open(mapObj, marker);
+            }
+            
+        }(myMap, markr,getDescription(geoLocationArray[count])));
+    }
+    
+}
+
+
+/*
+This is a method that initializes google maps in the web page
+intentionally hoisted the function. 
+
+1) Init will render the map first.
+2) then it will take the points on the Array of geo locations and pin them onto the map.
+3) Add description to the geo tags so that the user can change the status there itself.
+*/
+
+function initialize() {
+
+
+    var mapCanvas = document.getElementById('map_canvas');
+
+    var mapOptions = {
+        //12.58,77.38
+        center: new google.maps.LatLng(12.9667, 77.5667),
+        zoom: 10,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    }
+    var map = new google.maps.Map(mapCanvas, mapOptions);
+
+
+
+    pinCoordinates(map,locationArray);
+};
+
+
+
+
+
+/*
+End addition by Harsha
+*/
+
+
+
 //logout
 //Parse.User.logOut();
 function getAllData() {
@@ -29,6 +191,9 @@ function getAllData() {
 function appendAllData(data) {
     var results = data.results;
     var size = results.length;
+    
+    //Harsha: MAking the array global for good
+    locationArray = results;
     
     for(var i = 0; i < size; i++) {
         if(results[i].isComplete)
@@ -140,3 +305,7 @@ function signUp(buttonId) {
         }
     });
 }
+
+
+//Render Google Maps
+google.maps.event.addDomListener(window, 'load', initialize);
